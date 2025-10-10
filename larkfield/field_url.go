081ff71509d40e454c7c200a6d87ed0972bb57c0
andy <andycylin211@gmail.com>
2025-10-10 22:34:44 +0800

    url condition

--- larkfield/field_url.go
+++ larkfield/field_url.go
@@ -7,3 +7,27 @@ type UrlField struct {
 func (f *UrlField) SetValue(v string) {
 	f.SetUnderlayValue(v)
 }
+
+func (f *UrlField) Is(value string) *Condition {
+	return conditionIs(f.id, f.name, value)
+}
+
+func (f *UrlField) IsNot(value string) *Condition {
+	return conditionIsNot(f.id, f.name, value)
+}
+
+func (f *UrlField) Contains(value string) *Condition {
+	return conditionContains(f.id, f.name, value)
+}
+
+func (f *UrlField) DoesNotContains(value string) *Condition {
+	return conditionDoesNotContains(f.id, f.name, value)
+}
+
+func (f *UrlField) IsEmpty() *Condition {
+	return conditionIsEmpty(f.id, f.name)
+}
+
+func (f *UrlField) IsNotEmpty() *Condition {
+	return conditionIsNotEmpty(f.id, f.name)
+}
