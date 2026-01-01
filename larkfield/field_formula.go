4d5797753ac2482a5920dab90d5167925977c0b9
lincaiyong <lincaiyong@bytedance.com>
2026-01-01 14:20:26 +0800

    larkbase

--- larkfield/field_formula.go
+++ larkfield/field_formula.go
@@ -3,3 +3,10 @@ package larkfield
 type FormulaField struct {
 	BaseField
 }
+
+func (f *FormulaField) IsEmpty() *Condition {
+	return conditionIsEmpty(f.id, f.name)
+}
+func (f *FormulaField) IsNotEmpty() *Condition {
+	return conditionIsNotEmpty(f.id, f.name)
+}
--- larkfield/field_lookup.go
+++ larkfield/field_lookup.go
@@ -3,3 +3,10 @@ package larkfield
 type LookupField struct {
 	BaseField
 }
+
+func (f *LookupField) IsEmpty() *Condition {
+	return conditionIsEmpty(f.id, f.name)
+}
+func (f *LookupField) IsNotEmpty() *Condition {
+	return conditionIsNotEmpty(f.id, f.name)
+}
